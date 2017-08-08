7c44364149630e3a3359f55135ebc2a94968681e
Roxanne Charchian <rmcharchian@gmail.com>
2017-08-08 13:38:38 -0500

    Completed 2-5

--- solution.sql
+++ solution.sql
@@ -1,2 +1,12 @@
 --1 SELECT * FROM syntax_practice WHERE city = 'chicago';
---2
+
+--2 SELECT * FROM syntax_practice WHERE username ILIKE '%a%';
+
+--3 UPDATE syntax_practice SET account_balance = 10.00 WHERE account_balance = 0.00
+--3 AND transactions_attempted = 0;
+
+--4 SELECT * FROM syntax_practice WHERE transactions_attempted >= 9;
+
+--5  SELECT * FROM syntax_practice ORDER BY account_balance DESC LIMIT 3;
+
+--6  
